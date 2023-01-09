5b840d12097b4f7af876e3477445234a541ea3a8
Dennys Hsieh <dennys@users.noreply.github.com>
2023-01-09 10:42:27 +0800

    1. Extend the range of movement of the target.

--- main.ts
+++ main.ts
@@ -7,26 +7,26 @@ input.onButtonPressed(Button.AB, function () {
     bullet.turn(Direction.Left, 90)
     for (let index = 0; index < 4; index++) {
         bullet.move(1)
-        basic.pause(200)
-    }
-    if (bullet.isTouching(banana)) {
-        banana.delete()
-        bullet.delete()
-        game.addScore(1)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
-        strip.showRainbow(1, 360)
-        for (let index = 0; index < 30; index++) {
-            strip.show()
-            strip.rotate(1)
-            basic.pause(100)
+        if (bullet.isTouching(banana)) {
+            bullet.delete()
+            banana.delete()
+            game.addScore(1)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
+            strip.showRainbow(1, 360)
+            for (let index = 0; index < 30; index++) {
+                strip.show()
+                strip.rotate(1)
+                basic.pause(100)
+            }
+            basic.pause(1000)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+            strip.clear()
+            banana = game.createSprite(randint(0, 3), 0)
+        } else {
+            basic.pause(200)
         }
-        basic.pause(1000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        strip.clear()
-        banana = game.createSprite(randint(0, 3), 0)
-    } else {
-        bullet.delete()
     }
+    bullet.delete()
 })
 input.onButtonPressed(Button.B, function () {
     monkey.change(LedSpriteProperty.X, 1)
@@ -43,5 +43,6 @@ game.setScore(0)
 game.startCountdown(50000)
 basic.forever(function () {
     basic.pause(randint(500, 5000))
-    banana.set(LedSpriteProperty.X, randint(0, 3))
+    banana.set(LedSpriteProperty.X, randint(0, 4))
+    banana.set(LedSpriteProperty.Y, randint(0, 2))
 })
